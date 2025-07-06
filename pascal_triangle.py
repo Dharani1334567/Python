a178b50b3f55e984cf687e58f30709a2ddcb1e23
Dharani1334567 <162035096+Dharani1334567@users.noreply.github.com>
2025-07-06 22:14:53 +0530

    Add files via upload

---
+++ pascal_triangle.py
@@ -0,0 +1,10 @@
+r=int(input("enter:"))
+
+for i in range(r):
+    n=1
+    for j in range(r-i-1):
+        print(" ",end=" ")
+    for k in range(i +1):
+        print(n,end="   ")
+        n=n*(i-k)//(k+1)
+    print()
